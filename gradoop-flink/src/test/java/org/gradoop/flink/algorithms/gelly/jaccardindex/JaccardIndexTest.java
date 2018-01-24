a713aee376bd326f0545eeacb2c03eb988464ee8
aehm03 <moritz.wilke@informatik.uni-leipzig.de>
2018-01-24 12:40:07 +0100

    added mirrored edges and result number format to test

--- gradoop-flink/src/test/java/org/gradoop/flink/algorithms/gelly/jaccardindex/JaccardIndexTest.java
+++ gradoop-flink/src/test/java/org/gradoop/flink/algorithms/gelly/jaccardindex/JaccardIndexTest.java
@@ -7,51 +7,68 @@ import org.junit.Test;
 public class JaccardIndexTest extends GradoopFlinkTestBase {
 
   /**
-   * Testing the Jaccard Index on a graph that mimics the undirected simple graph from the gelly
-   * unit tests.
-   * Undirected Simple Graph from: https://github
+   * Testing the JaccardIndex implementation on a graph that mimics the undirected simple graph
+   * from the gelly unit tests.
+   * Undirected Simple Graph from: {@see https://github
    * .com/apache/flink/blob/master/flink-libraries/flink-gelly/src/test/java/org/apache/flink
-   * /graph/asm/AsmTestBase.java
-   * Expected Results from: https://github
+   * /graph/asm/AsmTestBase.java}
+   * Expected Results from: {@see https://github
    * .com/apache/flink/blob/master/flink-libraries/flink-gelly/src/test/java/org/apache/flink
-   * /graph/library/similarity/JaccardIndexTest.java
+   * /graph/library/similarity/JaccardIndexTest.java}
    */
   @Test
-  public void testUndirectedSimpleGraph() {
-    String graph =
-      "input[" + "(v0) --> (v1) " + "(v0) --> (v2) " + "(v2) --> (v1) " + "(v2) --> (v3) " +
-        "(v3) --> (v1) " + "(v3) --> (v4) " + "(v5) --> (v3) " + "]";
+  public void testUndirectedSimpleGraph() throws Exception {
 
-    String results = "input[" +
+    // Two mirrored directed edges represent one undirected edge
+    String inputString = "(v0:v0) --> (v1:v1) (v1) --> (v0)" + "(v0) --> (v2:v2) (v2) --> (v0)" +
+      "(v2) --> (v1) (v1) --> (v2)" + "(v2) --> (v3:v3) (v3) --> (v2)" +
+      "(v3) --> (v1)  (v1) --> (v3)" + "(v3) --> (v4:v4) (v4) --> (v3)" +
+      "(v5:v5) --> (v3) (v3) --> (v5)";
+
+    String expectedResultString = "input[" + inputString +
       // "(0,1,1,4)\n" +
-      "(v0) -[:JaccardIndex {value : 0.25}]-> (v1) " +
+      "(v0) -[:JaccardIndex {value : 0.25d}]-> (v1) " +
+      "(v1) -[:JaccardIndex {value : 0.25d}]-> (v0) " +
       // "(0,2,1,4)\n" +
-      "(v0) -[:JaccardIndex {value : 0.25}]-> (v2) " +
+      "(v0) -[:JaccardIndex {value : 0.25d}]-> (v2) " +
+      "(v2) -[:JaccardIndex {value : 0.25d}]-> (v0) " +
       // 			"(0,3,2,4)\n" +
-      "(v0) -[:JaccardIndex {value : 0.5}] -> (v3) " +
+      "(v0) -[:JaccardIndex {value : 0.5d}] -> (v3) " +
+      "(v3) -[:JaccardIndex {value : 0.5d}] -> (v0) " +
       // 			"(1,2,2,4)\n" +
-      "(v1) -[:JaccardIndex {value : 0.5}] -> (v2) " +
+      "(v1) -[:JaccardIndex {value : 0.5d}] -> (v2) " +
+      "(v2) -[:JaccardIndex {value : 0.5d}] -> (v1) " +
       // "(1,3,1,6)\n" +
-      "(v1) -[:JaccardIndex {value : 0.1666}]-> (v3) " +   // TODO precision??
+      "(v1) -[:JaccardIndex {value : 0.16666666666666666d}]-> (v3) " +
+      "(v3) -[:JaccardIndex {value : 0.16666666666666666d}]-> (v1) " +
       // "(1,4,1,3)\n" +
-      "(v1) -[:JaccardIndex {value : 0.3333}]-> (v4) " +
+      "(v1) -[:JaccardIndex {value : 0.3333333333333333d}]-> (v4) " +
+      "(v4) -[:JaccardIndex {value : 0.3333333333333333d}]-> (v1) " +
       // "(1,5,1,3)\n" +
-      "(v1) -[:JaccardIndex {value : 0.3333}] -> (v5) " +
+      "(v1) -[:JaccardIndex {value : 0.3333333333333333d}] -> (v5) " +
+      "(v5) -[:JaccardIndex {value : 0.3333333333333333d}] -> (v1) " +
       // "(2,3,1,6)\n" +
-      "(v2) -[:JaccardIndex {value : 0.1666}]-> (v3) " +
+      "(v2) -[:JaccardIndex {value : 0.16666666666666666d}]-> (v3) " +
+      "(v3) -[:JaccardIndex {value : 0.16666666666666666d}]-> (v2) " +
       // "(2,4,1,3)\n" +
-      "(v2) -[:JaccardIndex {value : 0.3333}]-> (v4) " +
+      "(v2) -[:JaccardIndex {value : 0.3333333333333333d}]-> (v4) " +
+      "(v4) -[:JaccardIndex {value : 0.3333333333333333d}]-> (v2) " +
       // "(2,5,1,3)\n" +
-      "(v2) -[:JaccardIndex {value : 0.3333}]-> (v5) " +
+      "(v2) -[:JaccardIndex {value : 0.3333333333333333d}]-> (v5) " +
+      "(v5) -[:JaccardIndex {value : 0.3333333333333333d}]-> (v2) " +
       // 	"(4,5,1,1)\n";
-      "(v4) -[:JaccardIndex {value : 1}]-> (v5) " + "]";
+      "(v4) -[:JaccardIndex {value : 1.0d}]-> (v5) " +
+      "(v5) -[:JaccardIndex {value : 1.0d}]-> (v4) " + "]";
 
-    LogicalGraph simpleGraph = getLoaderFromString(graph).getLogicalGraphByVariable("input");
-    LogicalGraph expectedResult = getLoaderFromString(results).getLogicalGraphByVariable("input");
+    LogicalGraph input =
+      getLoaderFromString("input[" + inputString + "]").getLogicalGraphByVariable("input");
 
-    LogicalGraph result = simpleGraph.callForGraph(new JaccardIndex());
+    LogicalGraph expectedResult =
+      getLoaderFromString(expectedResultString).getLogicalGraphByVariable("input");
 
-    expectedResult.equalsByData(result);
+    LogicalGraph result = input.callForGraph(new JaccardIndex("JaccardIndex"));
+
+    collectAndAssertTrue(expectedResult.equalsByElementData(result));
 
   }
 }
